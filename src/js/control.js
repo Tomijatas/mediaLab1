dd78642cfc354ba5cae5b45da08ad5cf181a3940
Tomijatas <t.petruskevicius@stud.vilniustech.lt>
2026-04-16 22:34:13 +0300

    Pakeistas absoliutus kelias į .js failą.

--- src/js/control.js
+++ src/js/control.js
@@ -1,27 +1,36 @@
-const player = videojs('video');
+let player;
 
+document.addEventListener("DOMContentLoaded", function () {
+  player = videojs('video');
+});
 
+// ▶ Play
 function playVideo() {
+  if (!player) return;
   player.play();
 }
 
-
+// ⏸ Pause
 function pauseVideo() {
+  if (!player) return;
   player.pause();
 }
 
-
+// ⏹ Stop
 function stopVideo() {
+  if (!player) return;
   player.pause();
   player.currentTime(0);
 }
 
-
+// +5 sec
 function forward() {
+  if (!player) return;
   player.currentTime(player.currentTime() + 5);
 }
 
-
+// -5 sec
 function backward() {
+  if (!player) return;
   player.currentTime(player.currentTime() - 5);
 }
