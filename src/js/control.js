5714e6a321d8db3772beb87288f67f38940b2abc
Tomijatas <t.petruskevicius@stud.vilniustech.lt>
2026-04-16 22:23:06 +0300

    Papildytas funkcionalumas play, stop pause funkcijomis

--- src/js/control.js
+++ src/js/control.js
@@ -1,29 +1,27 @@
-<script>
 const player = videojs('video');
 
-// ▶ Play
+
 function playVideo() {
   player.play();
 }
 
-// ⏸ Pause
+
 function pauseVideo() {
   player.pause();
 }
 
-// ⏹ Stop
+
 function stopVideo() {
   player.pause();
   player.currentTime(0);
 }
 
-// ⏩ +5 sec
+
 function forward() {
   player.currentTime(player.currentTime() + 5);
 }
 
-// ⏪ -5 sec
+
 function backward() {
   player.currentTime(player.currentTime() - 5);
 }
-</script>
